74c2d74e8bb9b606e1cf6f7dfa368b8affad5a09
neon333 <ryska.v@gmail.com>
2019-05-03 15:17:03 +0200

    Domain project with Product Entity added

---
+++ WebApi.Domain/Product.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace WebApi.Domain
+{
+    public class Product
+    {
+        [Required]
+        public int Id { get; set; }
+        [Required]
+        public string Name { get; set; }
+        [Required]
+        [DataType(DataType.ImageUrl)]
+        [Url]
+        public Uri ImgUri { get; set; }
+        [Required]
+        [DataType(DataType.Currency)]
+        public decimal Price { get; set; }
+        public string Description { get; set; }
+    }
+}
